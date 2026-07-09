a0cac18dd8c66049ffd54c7f41a6f668aecf83c0
Simmeow Together <partnerships@simmeowtogether.com>
2026-07-09 10:02:03 +0700

    Version 0.6 - Component architecture

---
+++ src/data/site.ts
@@ -0,0 +1,33 @@
+export const site = {
+  name: "Simmeow Together",
+
+  tagline: "Gaming Adventures Begin Here",
+
+  email: "partnerships@simmeowtogether.com",
+
+  youtube: "https://youtube.com/@simmeowtogether",
+
+  facebook: "https://facebook.com/Simmeow2gether",
+
+  tiktok: "https://tiktok.com/@simmeow2gether",
+
+  location: "Bangkok, Thailand",
+
+  subscribers: "190K+",
+
+  views: "20M+",
+
+  audience: "Burmese Gaming Community"
+};
+
+export const sponsors = [
+  "logitech",
+  "netease",
+  "oncehuman",
+  "pubg",
+  "funcom",
+  "thqnordic",
+  "wherewindsmeet",
+  "youdao",
+  "thefunpimps"
+];
